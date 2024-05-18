764335712a114434e43efbfb69e90d1d595cbfab
Govinda Mandal <govinda4india@gmail.com>
2024-05-18 14:57:47 +0530

    Added logo

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -3,29 +3,22 @@
     <main-container>
       <q-header class="hyperhire-header">
         <div class="site-name">
-          Hyperhire
+          <logo-hyperhire />
         </div>
         <div class="header-menu">
-          <div class="header-menu-child">
-            Menu 1
-          </div>
-          <div class="header-menu-child">
-            Menu 2
-          </div>
+          <div class="header-menu-child">Menu 1</div>
+          <div class="header-menu-child">Menu 2</div>
         </div>
-        <div class="profile">
-          Profile Button
-        </div>
+        <div class="profile">Profile Button</div>
       </q-header>
       <q-page-container>
         <router-view />
       </q-page-container>
     </main-container>
-    <q-footer>
-      Footer
-    </q-footer>
+    <q-footer> Footer </q-footer>
   </q-layout>
 </template>
 <script setup lang="ts">
+import LogoHyperhire from 'src/components/LogoHyperhire.vue';
 import MainContainer from './MainContainer.vue';
 </script>
