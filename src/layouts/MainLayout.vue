bbbcd851f2d742f18b6846ecfe0a4d39eb569017
Govinda Mandal <govinda4india@gmail.com>
2024-05-18 16:36:50 +0530

    Header section finished

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -6,10 +6,41 @@
           <logo-hyperhire />
         </div>
         <div class="header-menu">
-          <div class="header-menu-child">Menu 1</div>
-          <div class="header-menu-child">Menu 2</div>
+          <q-btn-dropdown
+            class="no-hover header-menu-child"
+            label="채용"
+            hoverable="false"
+          >
+            <q-list>
+              <q-item clickable v-close-popup>
+                <q-item-section class="text-bold cursor-auto">
+                  <q-item-label>채용</q-item-label>
+                </q-item-section>
+              </q-item>
+              <q-item clickable v-close-popup>
+                <q-item-section>
+                  <q-item-label>해외 개발자 원격 채용</q-item-label>
+                </q-item-section>
+              </q-item>
+
+              <q-item clickable v-close-popup>
+                <q-item-section>
+                  <q-item-label>외국인 원격 채용 (비개발 직군)</q-item-label>
+                </q-item-section>
+              </q-item>
+
+              <q-item clickable v-close-popup>
+                <q-item-section>
+                  <q-item-label>한국어 가능 외국인 채용</q-item-label>
+                </q-item-section>
+              </q-item>
+            </q-list>
+          </q-btn-dropdown>
+          <div class="header-menu-child">해외 개발자 활용 서비스</div>
         </div>
-        <div class="profile">Profile Button</div>
+        <div class="profile">
+          <q-btn color="white" text-color="black" label="문의하기" />
+        </div>
       </q-header>
       <q-page-container>
         <router-view />
